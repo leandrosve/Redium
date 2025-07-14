d54cd8952787c45077798c232c527bcd76f27cb3
Leandro <leandrosvetlich@gmail.com>
2025-07-14 10:22:14 -0300

    added i18n configuration

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,11 @@
+import { useTranslation } from 'react-i18next';
 import './App.css'
 
 function App() {
-
+ const { t } = useTranslation();
   return (
     <div>
+      {t('home.title')}
     </div>
   )
 }
