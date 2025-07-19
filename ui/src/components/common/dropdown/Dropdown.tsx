f3eb62626a2d111ca7f649329b8b5562ca809022
Leandro <leandrosvetlich@gmail.com>
2025-07-19 01:34:42 -0300

    fixed dropdown re-opening issue

--- ui/src/components/common/dropdown/Dropdown.tsx
+++ ui/src/components/common/dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, type ReactNode } from "react";
+import { createContext, useContext, useEffect, useRef, useState, type ReactNode } from "react";
 
 interface DropdownContextType {
   isOpen: boolean;
@@ -23,11 +23,36 @@ interface DropdownProps {
 
 export const Dropdown = ({ children, onSelect }: DropdownProps) => {
   const [isOpen, setIsOpen] = useState(false);
+
   const toggle = () => setIsOpen((prev) => !prev);
   const close = () => setIsOpen(false);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!isOpen) {
+      document.documentElement.classList.remove("no-scroll-menu");
+      return;
+    }
+    const handleClickOutside = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        close();
+      }
+    };
+    document.documentElement.classList.add("no-scroll-menu");
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.documentElement.classList.remove("no-scroll-menu");
+    };
+  }, [isOpen, close]);
+  
   return (
     <DropdownContext.Provider value={{ isOpen, toggle, close, onSelect }}>
-      <div className="relative inline-block">{children}</div>
+      <div className="relative inline-block" ref={ref}>
+        {children}
+      </div>
     </DropdownContext.Provider>
   );
 };
--- ui/src/components/common/dropdown/DropdownMenu.tsx
+++ ui/src/components/common/dropdown/DropdownMenu.tsx
@@ -12,32 +12,10 @@ interface DropdownMenuProps {
 export const DropdownMenu = ({ children, className, position = "left" }: DropdownMenuProps) => {
   const { isOpen, close } = useDropdown();
 
-  const ref = useRef<HTMLUListElement>(null);
-
-  useEffect(() => {
-    if (!isOpen) {
-      document.documentElement.classList.remove("no-scroll-menu");
-      return;
-    }
-    const handleClickOutside = (e: MouseEvent) => {
-      if (ref.current && !ref.current.contains(e.target as Node)) {
-        close();
-      }
-    };
-    document.documentElement.classList.add("no-scroll-menu");
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-      document.documentElement.classList.remove("no-scroll-menu");
-    };
-  }, [isOpen, close]);
-
   if (!isOpen) return null;
 
   return (
     <ul
-      ref={ref}
       className={join(
         "absolute mt-2 border top-full rounded-xl shadow z-10 bg-content-100 dark:bg-base backdrop-blur-3xl border-subtle overflow-hidden",
         position == "right" ? "left-0" : "right-0",
